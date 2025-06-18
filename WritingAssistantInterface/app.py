22f24456e7cc088fee0482cfaecadcec63b06f7c
Philippe-Lauwers <philippe.lauwers@gmail.com>
2025-06-18 22:38:39 +0200

    Little tweak to the Interface as installs tend to mess with things

--- WritingAssistantInterface/app.py
+++ WritingAssistantInterface/app.py
@@ -13,14 +13,6 @@ app = Flask(
   template_folder=os.path.join(BASE, 'templates'),
 )
 
-BACKEND_URL = 'http://localhost:5050'
-
-## Handle to database
-load_dotenv(os.path.join(BASE,'..','.env'))  # reads .env and populates os.environ
-print("Using DATABASE_URL:", os.getenv('DATABASE_URL'))
-# This line tells SQLAlchemy where to find your database:
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
